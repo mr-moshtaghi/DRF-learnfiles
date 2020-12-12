02f6680ffdf9f0f64372845e6975d3ad82594e02
mr-moshtaghi <mr.ahmadalimoshtaghi@gmail.com>
2020-12-12 08:45:22 +0330

    update serializers.py

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 
-class ProfileSerializers(serializers.Serializer):
-    phone_number = serializers.CharField(max_length=11)
-    first_name = serializers.CharField(max_length=50)
-    last_anme = serializers.CharField(max_length=70)
+from .models import Profiles
+
+class ProfileSerializers(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Profiles
+        feilds = "__all__"
