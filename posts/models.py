7f7ee7f0cd761795094782f5cd2225d2f323822d
mr-moshtaghi <mr.ahmadalimoshtaghi@gmail.com>
2020-12-12 16:40:09 +0330

    creat admin for apps post and profiles

--- posts/models.py
+++ posts/models.py
@@ -7,3 +7,6 @@ class Posts(models.Model):
     content = models.CharField(max_length=100)
     date = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(Profiles , related_name='posts' , on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- profiles/models.py
+++ profiles/models.py
@@ -6,3 +6,6 @@ class Profiles(models.Model):
     phone_number = models.CharField(max_length=11)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return u'%s - %s' % (self.first_name, self.last_name)
