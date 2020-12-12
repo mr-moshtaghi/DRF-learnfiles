9e4a32dc935bd2f7d5dde6269f667d140239b20c
mr-moshtaghi <mr.ahmadalimoshtaghi@gmail.com>
2020-12-12 15:59:38 +0330

    add db.sqlite3 and create app posts for nested serializer

---
+++ posts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+from profiles.models import Profiles
+
+class Posts(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(Profiles , related_name='posts' , on_delete=models.CASCADE)
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,8 +3,11 @@ import re
 from rest_framework.exceptions import ValidationError
 
 from .models import Profiles
+from posts.serializers import PostsSerializers
 
 class ProfileSerializers(serializers.ModelSerializer):
+
+    posts = PostsSerializers( many = True , read_only=True)
     
     class Meta:
         model = Profiles
